84e0c7a64ea8be8cedaeaa968803187766f5ccbd
Leandro <leandro.cruz.oliveira@hotmail.com>
2021-02-25 12:40:33 -0300

    Inicio
    25.02

---
+++ Assets/Codes/AlterarCena.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class AlterarCena : MonoBehaviour
+{
+    public void Menu(){
+        SceneManager.LoadScene("01_Menu");
+    }
+
+    public void Login(){
+        SceneManager.LoadScene("02_Login");
+    }
+
+    public void Parabens(){
+        SceneManager.LoadScene("07_Parabens");
+    }
+
+    public void Ranking(){
+        SceneManager.LoadScene("02_Ranking");
+    }
+
+
+
+    public void Entrar(){
+        /*  
+        if(user.idade <= 15 && user.cidade == Belém){
+            SceneManager.LoadScene("05_Menor15");
+        }
+
+        else if(user.idade >= 15 && user.cidade == Belém){
+            SceneManager.LoadScene("06_Maior15");
+        }
+        */
+
+        SceneManager.LoadScene("05_Menor15");
+    }
+}
---
+++ Assets/Codes/Cronometro.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Cronometro : MonoBehaviour{
+    public Text cronometroText;
+    private float contagem = 30.0f;
+    
+    void Update(){
+        if (contagem > 0.0f){
+            contagem -= Time.deltaTime;
+            cronometroText.text = contagem.ToString("F2");
+        }
+
+        else
+            cronometroText.text = "Acabou o tempo";
+    }
+}
